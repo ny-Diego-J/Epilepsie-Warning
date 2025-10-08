e17d50b419bbcd8d020d0a157d80cecf54fd75a5
Diego Jimenez <diego.jimenez@noseryoung.ch>
2025-10-08 14:03:39 +0200

    added webpage with epilepsie

---
+++ main.js
@@ -0,0 +1,47 @@
+function download() {
+  let random = Math.floor(Math.random() * 10);
+  if (random < 5) {
+    window.location.href = "no guy gets your unterstanding.exe";
+  } else {
+    window.location.href = "no guy gets your unterstanding1.exe";
+  }
+}
+
+function hover() {
+  let download = document.querySelector("img");
+  const windowWidth = window.innerWidth;
+  const windowHeight = window.innerHeight;
+  const imgWidth = download.offsetWidth;
+  const imgHeight = download.offsetHeight;
+
+  let randx = Math.floor(Math.random() * windowWidth);
+  let randy = Math.floor(Math.random() * windowHeight);
+
+  let randomy = Math.floor(Math.random() * 1000);
+  let randomx = Math.floor(Math.random() * 1000);
+
+  download.style.width = randomx + "px";
+  download.style.height = randomy + "px";
+  if (randx + imgWidth >= windowWidth) {
+    randx = windowWidth - imgWidth;
+  } else if (randx < 0) {
+    randx = 0;
+  } else if (randy + imgHeight >= windowHeight) {
+    randy = windowHeight - imgHeight;
+  } else if (randy < 0) {
+    randy = 0;
+  } else if (randx == download.style.left) {
+    randx = randx + 100;
+  } else if (randy == download.style.top) {
+    randy = randy + 100;
+  }
+
+  download.style.left = randx + "px";
+  download.style.top = randy + "px";
+  download.style.cursor = "pointer";
+  download.style.transitionDuration = "0.5s";
+}
+
+function download2() {
+  window.location.href = "CalcflexSetup.exe";
+}
